7cf78edd1675848809a7f08045b7c47e84c37980
kevin <kevinjobrien420@gmail.com>
2018-06-23 02:00:32 -0500

    242 App Number 30 - Part3 - Moving from one view to another View // Coding

--- app/src/main/java/com/example/kevin/movingfromoneviewtoanotherviewinandroid/MainActivity.java
+++ app/src/main/java/com/example/kevin/movingfromoneviewtoanotherviewinandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kevin.movingfromoneviewtoanotherviewinandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,6 +24,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override  // Whatever is in this onClick method will be executed
     public void onClick(View view) {
+
+        // Intent allows us to go from one activity to another activity...
+        Intent intent = new Intent();
+        intent.setClass(this, SecondActivity.class);
+
+        startActivity(intent);
 
     }
 }
--- app/src/main/java/com/example/kevin/movingfromoneviewtoanotherviewinandroid/SecondActivity.java
+++ app/src/main/java/com/example/kevin/movingfromoneviewtoanotherviewinandroid/SecondActivity.java
@@ -1,13 +1,14 @@
 package com.example.kevin.movingfromoneviewtoanotherviewinandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class second_activity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second_activity);
+        setContentView(R.layout.second_activity);
     }
 }
