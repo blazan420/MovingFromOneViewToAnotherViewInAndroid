9e688480e3f4dbd9dc83f50a5e8c6aa48e668153
kevin <kevinjobrien420@gmail.com>
2018-06-23 01:34:55 -0500

    241 App Number 30 - Part2 - Moving from one View to another View in Android // Coding

--- app/src/main/java/com/example/kevin/movingfromoneviewtoanotherviewinandroid/MainActivity.java
+++ app/src/main/java/com/example/kevin/movingfromoneviewtoanotherviewinandroid/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.kevin.movingfromoneviewtoanotherviewinandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSecondActivity = (Button) findViewById(R.id.btnSecondActivity);
+
+
+        btnSecondActivity.setOnClickListener(this);
+
+
+
+    }
+
+    @Override  // Whatever is in this onClick method will be executed
+    public void onClick(View view) {
+
     }
 }
